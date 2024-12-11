23e68f72b7d4bf96f47f1bae21ff454f54cc84f4
Om Lanke <oplanke@gmail.com>
2024-12-12 00:55:13 +0530

    login context interface

--- contexts/loginContext.ts
+++ contexts/loginContext.ts
@@ -1,6 +1,11 @@
 import { createContext } from "react";
 
-export const LoginContext = createContext({
+interface LoginContextInterface {
+  isLoggedIn: boolean | null;
+  setIsLoggedIn: (value: boolean) => void;
+}
+
+export const LoginContext = createContext<LoginContextInterface>({
   isLoggedIn: null,
   setIsLoggedIn: (value: boolean) => {},
 });
