4dd6e82867c579ccbc68cd53e469b62c2c768090
harshana15 <harshana555prajapati@gmail.com>
2025-01-04 01:37:14 +0530

    fixes

--- app/clientVideo.tsx
+++ app/clientVideo.tsx
@@ -1,29 +1,29 @@
-// import "expo-dev-client";
-// import React, { useState } from "react";
-// import { router } from "expo-router";
-// import AgoraUIKit, { AgoraUIKitProps } from "agora-rn-uikit";
+import "expo-dev-client";
+import React, { useState } from "react";
+import { router } from "expo-router";
+import AgoraUIKit, { AgoraUIKitProps } from "agora-rn-uikit";
 
-// const App = () => {
-//   const [videoCall, setVideoCall] = useState(true);
-//   const props: AgoraUIKitProps = {
-//     connectionData: {
-//       appId: "5174a666a50f45619981c7138b57e017",
-//       channel: "exercise",
-//     },
-//     rtcCallbacks: {
-//       EndCall: () => {
-//         setVideoCall(false);
-//         router.replace("/")
-//       },
-//     },
-//   };
+const App = () => {
+  const [videoCall, setVideoCall] = useState(true);
+  const props: AgoraUIKitProps = {
+    connectionData: {
+      appId: "5174a666a50f45619981c7138b57e017",
+      channel: "exercise",
+    },
+    rtcCallbacks: {
+      EndCall: () => {
+        setVideoCall(false);
+        router.replace("/")
+      },
+    },
+  };
 
-//   return videoCall ? (
-//     <AgoraUIKit
-//       connectionData={props.connectionData}
-//       rtcCallbacks={props.rtcCallbacks}
-//     />
-//   ) : null;
-// };
+  return videoCall ? (
+    <AgoraUIKit
+      connectionData={props.connectionData}
+      rtcCallbacks={props.rtcCallbacks}
+    />
+  ) : null;
+};
 
-// export default App;
+export default App;
